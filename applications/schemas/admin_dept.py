8385f18393981528cb985da7eda59b35a1d3dfb7
liyanpengTy <troyccn@foxmail.com>
2024-07-12 22:21:57 +0800

    增加user_realname序列化显示，方便前端页面展示

--- applications/schemas/admin_dept.py
+++ applications/schemas/admin_dept.py
@@ -1,9 +1,12 @@
 from flask_marshmallow.sqla import SQLAlchemyAutoSchema
 
 from applications.models import Dept
+from marshmallow import fields
 
 
 class DeptSchema(SQLAlchemyAutoSchema):
+    user_realname = fields.String(attribute='user.realname')
+
     class Meta:
         model = Dept
         load_instance = True
